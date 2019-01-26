db9ff79fdda42342d2d77404efec97c88a3b4198
Felipe Aranguiz <faranguiz575@gmail.com>
2019-01-25 21:18:25 -0300

    Update SimpleLimit example

--- example_bots/simple_limit/bot.py
+++ example_bots/simple_limit/bot.py
@@ -1,135 +1,94 @@
-from collections import namedtuple
-
 from trading_bots.bots import Bot
-from trading_bots.contrib.clients import Market, Side
-from trading_bots.contrib.clients import bitfinex, bitstamp, buda, kraken
+from trading_bots.conf import settings
+from trading_bots.contrib.models import Side, Money
+from trading_bots.contrib.exchanges import buda, bitfinex, bitstamp, kraken
 from trading_bots.contrib.converters.open_exchange_rates import OpenExchangeRates
-from trading_bots.utils import truncate_to
+from trading_bots.utils import truncate_money
+from decimal import Decimal
 
 
 class SimpleLimit(Bot):
     label = 'SimpleLimit'
     market_clients = [
+        buda.BudaMarket,
         bitfinex.BitfinexMarket,
         bitstamp.BitstampMarket,
         kraken.KrakenMarket,
     ]
 
     def _setup(self, config):
-        # Set market
-        self.market = Market(config['market'])
-        # Init variables
-        self.bid_price, self.ask_price = None, None
-        self.base_amount, self.quote_amount = None, None
         # Set buda trading client
-        self.buda = buda.BudaTrading(
-            self.market, dry_run=self.dry_run, timeout=self.timeout, logger=self.log, store=self.store)
-        # Set converter
-        self.converter = OpenExchangeRates(timeout=self.timeout)
+        client_params = dict(timeout=self.timeout)
+        self.buda = buda.BudaTrading(config['market'], client_params, self.dry_run, self.log, self.store)
         # Set reference market client
-        reference_config = config['reference']
-        self.reference = self._get_market_client(reference_config['name'], reference_config['market'])
-        assert self.reference.market.base == self.market.base
+        self.reference = self._get_market_client(config['reference']['name'], config['reference']['market'])
+        assert self.reference.market.base == self.buda.market.base
+        # Set converter
+        app_id = settings.credentials['OpenExchangeRates']['app_id']
+        self.converter = OpenExchangeRates(return_decimal=True, client_params=dict(app_id=app_id))
+        # Set price multipliers
+        self.buy_multiplier = Decimal(str(config['prices']['buy_multiplier']))
+        self.sell_multiplier = Decimal(str(config['prices']['sell_multiplier']))
+        # Set max amounts
+        self.max_base = Money(config['amounts']['max_base'], self.buda.market.base)
+        self.max_quote = Money(config['amounts']['max_quote'], self.buda.market.quote)
 
     def _algorithm(self):
         # Setup
         self.log.info(f'Preparing prices using {self.reference.name} {self.reference.market.code}')
-        self.prepare_prices()
+        ref_bid, ref_ask = self._get_reference_prices()
+        self.log.info(f'Reference prices on {self.reference.name}: Bid: {ref_bid} Ask: {ref_ask}')
+        # Set offset prices from reference and price multiplier
+        price_buy = truncate_money(ref_bid * self.buy_multiplier)
+        price_sell = truncate_money(ref_ask * self.sell_multiplier)
+        self.log.info(f'{self.buda.market} calculated prices: Buy: {price_buy} Sell: {price_sell}')
         # Cancel open orders
         self.log.info('Closing open orders')
-        self.cancel_orders()
+        self.buda.cancel_all_orders()
         # Get available balances
-        self.prepare_amounts()
-        # Start strategy
+        self.log.info(f'Preparing amounts')
+        # Fetch available balances
+        available_base = self.buda.wallets.base.fetch_balance().free
+        available_quote = self.buda.wallets.quote.fetch_balance().free
+        # Adjust amounts to max in config
+        amount_base = min(self.max_base, available_base)
+        amount_quote = min(self.max_quote, available_quote)
+        self.log.info(f'Amounts | Bid: {amount_base} | Ask: {amount_quote}')
+        # Get order buy and sell amounts
+        # *quote amount must be converted to base
+        amount_buy = truncate_money(Money(amount_quote / price_buy, self.buda.market.base))
+        amount_sell = truncate_money(amount_base)
+        self.log.info(f'Amounts | Buy {amount_buy} | Sell {amount_sell}')
+        # PLACE ORDERS
         self.log.info('Starting order deployment')
-        # Deploy orders
-        deploy_list = self.get_deploy_list()
-        self.deploy_orders(deploy_list)
+        if amount_buy >= self.buda.min_order_amount:
+            self.buda.place_limit_order(side=Side.BUY, amount=amount_buy, price=price_buy)
+        if amount_sell >= self.buda.min_order_amount:
+            self.buda.place_limit_order(side=Side.SELL, amount=amount_sell, price=price_sell)
 
     def _abort(self):
         self.log.error('Aborting strategy, cancelling all orders')
         try:
-            self.cancel_orders()
+            self.buda.cancel_all_orders()
         except Exception:
-            self.log.exception(f'Failed!, some orders might not be cancelled')
+            self.log.critical(f'Failed!, some orders might not be cancelled')
             raise
         else:
             self.log.info(f'All open orders were cancelled')
 
-    def prepare_prices(self):
-        self.log.info(f'Preparing prices')
-        prices_config = self.config['prices']
-        ref_bid, ref_ask = self._get_reference_prices()
-        self.log.info(f'{self.market.base} reference prices on {self.reference.name}: Bid: {ref_bid} Ask: {ref_ask}')
-        # Set offset prices from reference and price multiplier
-        self.bid_price = ref_bid * prices_config['buy_multiplier']
-        self.ask_price = ref_ask * prices_config['sell_multiplier']
-        self.log.info(f'{self.market} calculated prices: Bid: {self.bid_price} Ask: {self.ask_price}')
-
-    def prepare_amounts(self):
-        self.log.info(f'Preparing amounts')
-        # Get amounts from configs
-        amounts_config = self.config['amounts']
-        max_base = amounts_config['max_base']
-        max_quote = amounts_config['max_quote']
-        # Set final bid and ask amounts
-        self.base_amount = min(max_base, self.buda.wallets.base.get_available())
-        self.quote_amount = min(max_quote, self.buda.wallets.quote.get_available())
-        self.log.debug(' | '.join([
-            'Amounts',
-            f'Bid: {self.quote_amount} {self.market.quote}',
-            f'Ask: {self.base_amount} {self.market.base}'
-        ]))
-
-    def get_deploy_list(self):
-
-        Order = namedtuple('order', 'amount price side')
-        deploy_list = []
-
-        # Available is on quote currency when side is sell
-        def quote_to_base_amount(_amount, _price):
-            quote_amount = _amount / _price
-            return self.truncate_amount(quote_amount)
-
-        buy_order_price = self.truncate_price(self.bid_price)
-        buy_order_amount = quote_to_base_amount(self.quote_amount, buy_order_price)
-        sell_order_price = self.truncate_price(self.ask_price)
-        sell_order_amount = self.truncate_amount(self.base_amount)
-
-        if buy_order_amount > self.buda.min_order_amount:
-            deploy_list.append(Order(buy_order_amount, buy_order_price, Side.BUY))
-        if sell_order_amount > self.buda.min_order_amount:
-            deploy_list.append(Order(sell_order_amount, sell_order_price, Side.SELL))
-        return deploy_list
-
-    def deploy_orders(self, deploy_list: list):
-        self.log.info(f'Deploying {len(deploy_list)} new orders')
-        for order in deploy_list:
-            self.buda.place_limit_order(
-                side=order.side,
-                amount=order.amount,
-                price=order.price,
-            )
-
-    def cancel_orders(self, remove_list: list=None):
-        remove_list = remove_list or []
-        if len(remove_list) > 0:
-            self.log.info(f'Canceling {len(remove_list)} orders')
-        else:
-            self.log.info(f'Canceling open orders')
-        self.buda.cancel_orders(remove_list)
-
     def _get_reference_prices(self):
-        ref_bid, ref_ask = self.reference.get_spread_details()
+        ticker = self.reference.fetch_ticker()
+        ref_bid, ref_ask = ticker.bid, ticker.ask
         # Convert reference_price if reference market differs from current market
-        if self.reference.market != self.market:
+        if self.reference.market != self.buda.market:
             # Get conversion rate (eg CLP/USD from OpenExchangeRates)
-            rate = self.converter.get_rate_for(self.reference.market.quote, self.market.quote)
-            self.log.info(f'{self.reference.market.quote}/{self.market.quote} rate: {rate} from {self.converter.name}')
+            rate = self.converter.get_rate_for(self.reference.market.quote, self.buda.market.quote)
+            self.log.info(f'{self.reference.market.quote}/{self.buda.market.quote} rate: {rate:.2f}'
+                          f' from {self.converter.name}')
             # Get market price according to reference (eg BTC/CLP converted from converter's BTC/USD)
-            ref_bid *= rate
-            ref_ask *= rate
-        return ref_ask, ref_ask
+            ref_bid = Money(ref_bid.amount * rate, self.buda.market.quote)
+            ref_ask = Money(ref_ask.amount * rate, self.buda.market.quote)
+        return ref_bid, ref_ask
 
     def _get_market_client(self, name, market):
         for client in self.market_clients:
@@ -137,9 +96,3 @@ class SimpleLimit(Bot):
                 return client(market, client=None, dry_run=self.dry_run,
                               timeout=self.timeout, logger=self.log, store=self.store)
         raise NotImplementedError(f'Client {name} not found!')
-
-    def truncate_amount(self, value):
-        return truncate_to(value, self.market.base)
-
-    def truncate_price(self, value):
-        return truncate_to(value, self.market.quote)
